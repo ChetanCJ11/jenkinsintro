acd214c6eab6e752385beeb8ac6b30f5c11f16e9
Chetan <chetanrj11@gmail.com>
2020-10-02 13:46:27 +0530

    Ädding new file

---
+++ hello.java
@@ -0,0 +1,15 @@
+public class hello{
+
+	public static void main(String[] args) {
+		
+		
+			
+		System.out.println("I m chetan");
+		System.out.println("I like to travell");
+		
+		System.out.println("***************************");
+		System.out.print("My native place");
+		System.out.print("  malavali");
+
+	}
+}
